9e5f2925e218b5afafe26e4a21ba1b1ea0c346c2
Hau Vo <hau.vophuoc@gmail.com>
2016-12-17 17:53:53 +0700

    Configure Eslint and custom command for run ios & android

--- android/app/src/main/java/com/rnboilerplate/MainApplication.java
+++ android/app/src/main/java/com/rnboilerplate/MainApplication.java
@@ -1,44 +1,21 @@
-package com.rnboilerplate;
+package com.example;
 
-import android.app.Application;
-import android.util.Log;
+import android.support.annotation.Nullable;
 
-import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+public class MainApplication extends NavigationApplication {
     @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
     }
 
+    @Nullable
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new NavigationReactPackage()
-      );
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return null;
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
