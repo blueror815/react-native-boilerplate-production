862e27a0fce5f8c1f10956324468d5ea7661a73e
Hau Vo <hau.vophuoc@gmail.com>
2016-12-18 15:56:42 +0700

    Add redux devtools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import reducers from 'app/redux/reducers';
 import clientMiddleware from 'app/redux/middlewares/ApiClientMiddleware';
 import ApiClient from 'app/utils/ApiClient';
@@ -7,6 +8,6 @@ export default function createStoreWithMiddlewares() {
   const client = new ApiClient();
   const middleware = [clientMiddleware(client)];
 
-  const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
-  return createStoreWithMiddleware(reducers);
+  // use Command + Ctrl + Arrow Up to open remote redux dev tools
+  return createStore(reducers, composeWithDevTools(applyMiddleware(...middleware)));
 }
