24b76f12d422122b3780de19bd7e565288c1dc1b
Hau Vo <hau.vophuoc@gmail.com>
2016-12-18 17:18:55 +0700

    Fix lint error in FormScreen

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,13 +1,12 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   View,
   StyleSheet,
-  TextInput,
   TouchableHighlight
 } from 'react-native';
-import { Screens } from './index';
 import t from 'tcomb-form-native';
+
 const Form = t.form.Form;
 
 const styles = StyleSheet.create({
@@ -33,7 +32,7 @@ const styles = StyleSheet.create({
 });
 
 // here we are: define your domain model
-var Person = t.struct({
+const Person = t.struct({
   name: t.String,              // a required string
   surname: t.maybe(t.String),  // an optional string
   age: t.Number,               // a required number
@@ -44,7 +43,7 @@ export default class FormScreen extends Component {
 
   onPress = () => {
     // call getValue() to get the values of the form
-    var value = this.refs.form.getValue();
+    const value = this._form.getValue();
     if (value) { // if validation fails, value will be null
       console.log(value); // value here is an instance of Person
     }
@@ -54,10 +53,10 @@ export default class FormScreen extends Component {
     return (
       <View style={styles.container}>
         <Form
-          ref="form"
+          ref={(ref) => { this._form = ref; }}
           type={Person}
         />
-        <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+        <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor="#99d9f4">
           <Text style={styles.buttonText}>Save</Text>
         </TouchableHighlight>
       </View>
